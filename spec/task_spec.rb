240a05a4b2e7c02b98449757d8dd7aa8ad88887b
Rafael George <george.rafael@gmail.com>
2011-08-23 10:09:28 -0400

    added single task retrieval spec

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -5,6 +5,7 @@ describe "BugherdAPI::Task" do
     BugherdAPI.authenticate('user@email.com', '123456')
     ActiveResource::HttpMock.respond_to do |mock|
       mock.get '/api_v1/projects/1458/tasks.xml', {}, fixture_for('tasks', 'xml'), 200
+      mock.get '/api_v1/projects/1458/tasks/4950.xml', {}, fixture_for('tasks/4950', 'xml'), 200
     end
     @project_id = 1458
   end
@@ -13,6 +14,11 @@ describe "BugherdAPI::Task" do
     it "should retrieve all tasks" do 
       tasks = BugherdAPI::Task.find(:all, :params => { :project_id => @project_id })
       tasks.should_not be_nil
+    end
+
+    it "should retrieve a single task" do 
+      task = BugherdAPI::Task.find(4950, :params => { :project_id => @project_id })
+      task.should_not be_nil
     end
   end
 end

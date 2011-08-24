f3597b577a6fbab298182b2e2716eded02f755cc
Rafael George <george.rafael@gmail.com>
2011-08-24 18:12:25 -0400

    fixed email setting problem

--- lib/bugherd.rb
+++ lib/bugherd.rb
@@ -3,24 +3,34 @@ $: << File.dirname(__FILE__) unless $:.include?(File.dirname(__FILE__))
 require 'rubygems'
 require 'active_support'
 require 'active_resource'
+require 'bugherd/base'
 
 module BugherdAPI
   extend ActiveSupport::Autoload
 
-  autoload :Base,       'bugherd/base'
   autoload :User,       'bugherd/user'
   autoload :Project,    'bugherd/project'
   autoload :Task,       'bugherd/task'
   class Error < StandardError; end
 
   class << self
+    attr_accessor :email, :password
     def authenticate(email, password)
-      @email = email
-      @password = password
+      self.email = email
+      self.password = password
+      resources.each do |klass|
+        update_auth(klass)
+      end
     end
 
     def resources
       @resources ||= []
+    end
+
+    def update_auth(resource)
+      return unless email && password
+      resource.user     = email
+      resource.password = password
     end
   end
 

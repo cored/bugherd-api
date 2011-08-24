6470277bbe6f808172373e34c3f5dfe703701fa9
Rafael George <george.rafael@gmail.com>
2011-08-24 18:38:53 -0400

    fixed failing tasks specs

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -3,9 +3,10 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 describe "BugherdAPI::Task" do 
   before(:each) do 
     BugherdAPI.authenticate('user@email.com', '123456')
+    headers = {'Authorization' => 'Basic dXNlckBlbWFpbC5jb206MTIzNDU2', 'Accept' => 'application/xml'}
     ActiveResource::HttpMock.respond_to do |mock|
-      mock.get '/api_v1/projects/1458/tasks.xml', {}, fixture_for('tasks', 'xml'), 200
-      mock.get '/api_v1/projects/1458/tasks/4950.xml', {}, fixture_for('tasks/4950', 'xml'), 200
+      mock.get '/api_v1/projects/1458/tasks.xml', headers, fixture_for('tasks', 'xml'), 200
+      mock.get '/api_v1/projects/1458/tasks/4950.xml', headers, fixture_for('tasks/4950', 'xml'), 200
     end
     @project_id = 1458
   end

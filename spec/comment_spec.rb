b540fdb9611e2186b66e8c0fb4138291fc73f4bf
Rafael George <george.rafael@gmail.com>
2011-08-29 14:04:32 -0400

    added spec for testing a single comment search

--- spec/comment_spec.rb
+++ spec/comment_spec.rb
@@ -5,6 +5,7 @@ describe "BugherdAPI::Comment" do
     headers = {'Authorization' => 'Basic dXNlckBlbWFpbC5jb206MTIzNDU=', 'Accept' => 'application/xml'}
     ActiveResource::HttpMock.respond_to do |mock|
       mock.get '/api_v1/projects/1458/tasks/4950/comments.xml', headers, fixture_for('comments', 'xml'), 200
+      mock.get '/api_v1/projects/1458/tasks/4950/comments/9760.xml', headers, fixture_for('comments/9760', 'xml'), 200
     end
     @project_id = 1458
     @task_id = 4950
@@ -14,6 +15,12 @@ describe "BugherdAPI::Comment" do
     it "should retrieve all comments" do 
       BugherdAPI.authenticate('user@email.com', '12345')
       comments = BugherdAPI::Comment.find(:all, :params => {:project_id => 1458, :task_id => 4950})
+      comments.should_not be_nil
+    end
+
+    it "should retrieve a single comment" do 
+      BugherdAPI.authenticate('user@email.com', '12345')
+      comments = BugherdAPI::Comment.find(9760, :params => {:project_id => 1458, :task_id => 4950})
       comments.should_not be_nil
     end
   end

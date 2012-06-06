c7297c66752456c234acc93274108a66244370d2
Rafael George <george.rafael@gmail.com>
2012-06-06 12:23:58 +0000

    Finish pending specs for creating and updating projects

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -1,26 +1,27 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "BugherdAPI::Project" do 
-  let(:bugherd_api) { BugherdAPI.authenticate('user@email.com', '123456') }
-  let(:headers) { {'Authorization' => 'Basic dXNlckBlbWFpbC5jb206MTIzNDU2', 'Accept' => 'application/xml'} }
+  let(:headers) { {'Authorization' => 'Basic dXNlckBlbWFpbC5jb206MTIzNDU2' } }
+  let(:headers_get) { headers.merge('Accept' => 'application/xml') }
+  let(:headers_post) { headers.merge('Content-Type' => 'application/xml') }
   let(:project_class) { BugherdAPI::Project }
 
   describe "Retrieving projects" do 
     before(:each) do 
       ActiveResource::HttpMock.respond_to do |mock|
-        mock.get '/api_v1/projects.xml', headers, fixture_for('projects'), 200
-        mock.get '/api_v1/projects/1458.xml', headers, fixture_for('projects/1458'), 200
+        mock.get '/api_v1/projects.xml', headers_get, fixture_for('projects'), 200
+        mock.get '/api_v1/projects/1458.xml', headers_get, fixture_for('projects/1458'), 200
       end
     end
 
     context "when calling #find(:all)" do 
-      subject { BugherdAPI::Project.find(:all) }
+      subject { project_class.find(:all) }
       it { should be_an_instance_of Array }
       it { should_not be_nil }
     end
 
     context "when calling #find(id)" do 
-      subject { BugherdAPI::Project.find(1458) }
+      subject { project_class.find(1458) }
       it { should_not be_nil }
       it { should be_an_instance_of project_class }
       it { subject.name.should be_eql 'clutchapptest' }
@@ -28,9 +29,28 @@ describe "BugherdAPI::Project" do
     end
   end
 
-  context "Project creation, update and delete" do 
-    it "should create a project"
-    it "should update a project"
-    it "should delete a project"
+  describe "CRUD behaviour for projects" do 
+    before(:each) do 
+      ActiveResource::HttpMock.respond_to do |mock|
+        mock.post '/api_v1/projects.xml', headers_post, '', 200
+        mock.get '/api_v1/projects/1458.xml', headers_get, fixture_for('projects/1458'), 200
+        mock.put '/api_v1/projects/1458.xml', headers_post, '', 200
+      end
+    end
+
+    context "when calling #new and then #save" do 
+      subject { project_class.new :name => 'clutchapptest', :devurl => 'http://clutchapptest.ticketmaster.com' }
+      it { should_not be_nil }
+      it { subject.save.should be_true }
+    end
+
+    context "when calling #save after chaging a project" do 
+      it "should update a project" do 
+        p = project_class.find(1458)
+        p.should_not be_nil
+        p.name = 'new project'
+        p.save.should be_true
+      end
+    end
   end
 end
